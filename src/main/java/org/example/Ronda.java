06c8213e75e82f7bd20884a302984ad1c6f6a1ae
RodrigoAubry <rodri.aubry@gmail.com>
2023-04-08 04:00:21 -0300

    Primer envion generado con maven y reemplazado los getter/setter y constructores con lombok. no funciona

--- src/main/java/org/example/Ronda.java
+++ src/main/java/org/example/Ronda.java
@@ -8,13 +8,9 @@ public class Ronda {
     //Atributos
 
     private int nroRonda;
-    private Partido partidos[];
+    private Partido []partidos;
 
     //Metodos
-
-   /* public void setPartidos(Partido[] partidos) {
-        this.partidos = partidos;
-    }*/
 
     public int puntosTotales(Pronostico pron){
         pron.calcularPuntos();
